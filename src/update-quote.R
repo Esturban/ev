068d2ff3df1a36a26fb45a3a874b0f9eb073447b
Esturban <valtaye@gmail.com>
2021-06-26 23:20:36 +0100

    Quote CI/CD
    Testing GH action in dev to see if it works for updating the quote on my website
    
    Script worked as expected in testing locally

---
+++ src/update-quote.R
@@ -0,0 +1,19 @@
+require(jsonlite)
+
+#Load the config file
+config_file<-readLines("config.toml",encoding = "UTF-8")
+#Find the location of the footer file
+footer_loc<-grepl("footerText",config_file)
+#Clean up the footer text
+footert<-unlist(strsplit(config_file[footer_loc],"="))
+
+#Grab all quotes
+quotes<-jsonlite::fromJSON("data/quotes.json")
+#Get new quotes
+new_quote<-sample(quotes,1)
+# update footer
+new_footer<-paste0(footert[1]," = \"",new_quote,"\"")
+#Add footer to config file
+config_file[footer_loc]<-new_footer
+#Save the config file
+writeLines(config_file,con = "config.toml",useBytes = T)
